de1d92a487ec2e1eebccdb209a13abdaa7342db8
charleslzq <charleslzq@gmail.com>
2017-02-18 20:52:47 +0800

    重构testZip

--- ch01/ZipHacker/testZip.py
+++ ch01/ZipHacker/testZip.py
@@ -1,13 +1,24 @@
 import zipfile
+from threading import Thread
 
 
-z_file = zipfile.ZipFile('evil.zip')
-pass_file = open('dictionary.txt')
-for line in pass_file.readlines():
-    password = line.strip('\n')
+def extract_file(z_file, password):
     try:
         z_file.extractall(pwd=password.encode('utf-8'))
         print('[+] Password=' + password + '\n')
-        exit(0)
-    except Exception as e:
+    except:
         pass
+
+
+def main():
+    z_file = zipfile.ZipFile('evil.zip')
+    pass_file = open('dictionary.txt')
+    for line in pass_file.readlines():
+        password = line.strip('\n')
+        t = Thread(target=extract_file, args=(z_file, password))
+        t.start()
+
+
+if __name__ == '__main__':
+    main()
+
