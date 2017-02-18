c724f0d77f78332c407e4d00d65f3cf069cc4b11
charleslzq <charleslzq@gmail.com>
2017-02-18 21:01:57 +0800

    support for options

--- ch01/ZipHacker/testZip.py
+++ ch01/ZipHacker/testZip.py
@@ -1,4 +1,5 @@
 import zipfile
+import optparse
 from threading import Thread
 
 
@@ -11,8 +12,18 @@ def extract_file(z_file, password):
 
 
 def main():
-    z_file = zipfile.ZipFile('evil.zip')
-    pass_file = open('dictionary.txt')
+    parser = optparse.OptionParser("usage%prog -f <zipfile> -d <dictionary>")
+    parser.add_option('-f', dest='zname', type='string', help='specify zip file')
+    parser.add_option('-d', dest='dname', type='string', help='specify dictionary file')
+    (options, args) = parser.parse_args()
+    if (options.zname is None) | (options.dname is None):
+        print(parser.usage)
+        exit(0)
+    else:
+        zname = options.zname
+        dname = options.dname
+    z_file = zipfile.ZipFile(zname)
+    pass_file = open(dname)
     for line in pass_file.readlines():
         password = line.strip('\n')
         t = Thread(target=extract_file, args=(z_file, password))
